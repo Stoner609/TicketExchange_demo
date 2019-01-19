666607ebbfb9874cc2432e28e7f484c33f646b6c
Stoner609 <stoner7969@gmail.com>
2019-01-19 10:09:50 +0800

    feat:  新增 styled-somponents、node-sass 模組

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { AppDiv, AppLogo, AppHeader, AppLink } from "./AppCss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <AppDiv>
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
+          <AppLink
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-        </header>
-      </div>
+          </AppLink>
+        </AppHeader>
+      </AppDiv>
     );
   }
 }
---
+++ client/src/AppCss.js
@@ -0,0 +1,45 @@
+import styled, { keyframes } from "styled-components";
+
+const AppDiv = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+const AppLogo = styled.img`
+  animation: ${rotate} infinite 20s linear;
+  height: 40vmin;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
+
+// @keyframes App-logo-spin {
+//     from {
+//       transform: rotate(0deg);
+//     }
+//     to {
+//       transform: rotate(360deg);
+//     }
+//   }
+
+export { AppDiv, AppLogo, AppHeader, AppLink };
